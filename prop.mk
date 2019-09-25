d487a41b05e435276856289f32d72d6aee7392a3
laityts <laityts@gmail.com>
2019-09-25 22:18:47 +0800

    tiffany: PE-ify

--- prop.mk
+++ prop.mk
@@ -6,7 +6,7 @@ persist.vendor.audio.fluence.voicerec=false \
 ro.vendor.audio.sdk.fluencetype=fluence
 
 # Camera
-PRODUCT_PROPERTY_OVERRIDES += \
-vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap \
+vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,com.dual.GCam,com.Tele.GCam
 persist.camera.dual.camera=0
+persist.camera.eis.enable=1
 
