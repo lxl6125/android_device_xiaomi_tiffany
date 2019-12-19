25e9deceda9e975fd77c880c8e0893072802c184
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-12-19 21:33:11 +0000

    tiffany: Enable Google Recorder support

--- aosp_tiffany.mk
+++ aosp_tiffany.mk
@@ -24,6 +24,7 @@ $(call inherit-product, device/xiaomi/tiffany/device.mk)
 # Inherit some common PixelExperience stuff.
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
